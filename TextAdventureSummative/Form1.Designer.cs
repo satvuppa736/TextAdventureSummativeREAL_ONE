2cbb7e258e9a56eac8dc8797436fd6f687abdfdb
satvuppa736 <satvuppa736@ed.amdsb.ca>
2016-11-10 11:46:34 -0400

    Add project files.

---
+++ TextAdventureSummative/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace TextAdventureSummative
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.outputLabel = new System.Windows.Forms.Label();
+            this.redLabel = new System.Windows.Forms.Label();
+            this.blueLabel = new System.Windows.Forms.Label();
+            this.greenLabel = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // outputLabel
+            // 
+            this.outputLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold);
+            this.outputLabel.ForeColor = System.Drawing.Color.White;
+            this.outputLabel.Location = new System.Drawing.Point(16, 9);
+            this.outputLabel.Name = "outputLabel";
+            this.outputLabel.Size = new System.Drawing.Size(348, 198);
+            this.outputLabel.TabIndex = 2;
+            this.outputLabel.Text = "OUTPUT LABEL";
+            // 
+            // redLabel
+            // 
+            this.redLabel.AutoSize = true;
+            this.redLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.redLabel.ForeColor = System.Drawing.Color.White;
+            this.redLabel.Location = new System.Drawing.Point(66, 251);
+            this.redLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.redLabel.Name = "redLabel";
+            this.redLabel.Size = new System.Drawing.Size(40, 17);
+            this.redLabel.TabIndex = 3;
+            this.redLabel.Text = "RED";
+            // 
+            // blueLabel
+            // 
+            this.blueLabel.AutoSize = true;
+            this.blueLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.blueLabel.ForeColor = System.Drawing.Color.White;
+            this.blueLabel.Location = new System.Drawing.Point(66, 318);
+            this.blueLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.blueLabel.Name = "blueLabel";
+            this.blueLabel.Size = new System.Drawing.Size(48, 17);
+            this.blueLabel.TabIndex = 4;
+            this.blueLabel.Text = "BLUE";
+            // 
+            // greenLabel
+            // 
+            this.greenLabel.AutoSize = true;
+            this.greenLabel.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.greenLabel.ForeColor = System.Drawing.Color.White;
+            this.greenLabel.Location = new System.Drawing.Point(66, 374);
+            this.greenLabel.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.greenLabel.Name = "greenLabel";
+            this.greenLabel.Size = new System.Drawing.Size(62, 17);
+            this.greenLabel.TabIndex = 5;
+            this.greenLabel.Text = "GREEN";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(9, 432);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(122, 13);
+            this.label1.TabIndex = 6;
+            this.label1.Text = "TURN UP VOLUME!";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.2F, System.Drawing.FontStyle.Bold);
+            this.label2.ForeColor = System.Drawing.Color.White;
+            this.label2.Location = new System.Drawing.Point(135, 432);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(247, 13);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "| RED = M | BLUE = B | GREEN = SPACE |";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = global::TextAdventureSummative.Properties.Resources.red_50x50;
+            this.pictureBox1.Location = new System.Drawing.Point(9, 235);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(53, 58);
+            this.pictureBox1.TabIndex = 8;
+            this.pictureBox1.TabStop = false;
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Image = global::TextAdventureSummative.Properties.Resources.blue_50x50;
+            this.pictureBox2.Location = new System.Drawing.Point(9, 297);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(53, 56);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Image = global::TextAdventureSummative.Properties.Resources.green_50x50;
+            this.pictureBox3.Location = new System.Drawing.Point(9, 357);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(2);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(53, 54);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Black;
+            this.ClientSize = new System.Drawing.Size(376, 456);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.greenLabel);
+            this.Controls.Add(this.blueLabel);
+            this.Controls.Add(this.redLabel);
+            this.Controls.Add(this.outputLabel);
+            this.ForeColor = System.Drawing.Color.Black;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "Escape from the Lost Nation";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Label outputLabel;
+        private System.Windows.Forms.Label redLabel;
+        private System.Windows.Forms.Label blueLabel;
+        private System.Windows.Forms.Label greenLabel;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ TextAdventureSummative/Form1.cs
@@ -0,0 +1,407 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.Media;
+
+namespace TextAdventureSummative
+{
+    public partial class Form1 : Form
+    {
+        int scene = 0;
+        Random randGen = new Random();
+        int attackValue, stealthValue;
+
+        public Form1()
+        {
+            InitializeComponent();
+            outputLabel.Text = "You wake up in a dark room with a small trap door and a single torch. Do you go into the trap door or stay in the room?";
+            redLabel.Text = "LEAVE";
+            blueLabel.Text = "STAY";
+            greenLabel.Text = "";
+        }
+
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            /// check to see what button has been pressed and advance
+            /// to the next appropriate scene
+            if (e.KeyCode == Keys.M)       //red button press
+            {
+                if (scene == 0)
+                {
+                    scene = 1;
+                }
+
+                else if (scene == 1)
+                {
+                    scene = 3;
+                }
+
+                else if (scene == 3)
+                {
+                    Thread.Sleep(5000);
+                    Refresh();
+
+                    attackValue = randGen.Next(1, 11);
+                    if (attackValue >= 4)
+                    {
+                        scene = 6;
+                    }
+                    else if (attackValue < 4)
+                    {
+                        scene = 5;
+                    }
+                }
+                else if (scene == 5)
+                {
+                    scene = 26;
+                }
+                else if (scene == 6)
+                {
+                    scene = 7;
+                }
+                else if (scene == 7 || scene == 9)
+                {
+                    scene = 10;
+                }
+                else if (scene == 10)
+                {
+                    scene = 12;
+                }
+                else if (scene == 12 || scene == 14)
+                {
+                    scene = 15;
+                }
+                else if (scene == 15)
+                {
+                    scene = 17;
+                }
+                else if (scene == 17)
+                {
+                    scene = 18;
+                }
+                else if (scene == 18 || scene == 19 || scene == 21)
+                {
+                    scene = 23;
+                }
+                else if (scene == 24)
+                {
+                    scene = 16;
+                }
+
+                else if (scene == 26)
+                {
+                    scene = 0;
+                }
+                    
+            }
+            else if (e.KeyCode == Keys.B)  //blue button press
+            {
+                if (scene == 0)
+                {
+                    scene = 2;
+                }
+
+                else if (scene == 2)
+                {
+                    scene = 26;
+                }
+
+                else if (scene == 1)
+                {
+                    scene = 4;
+                }
+
+                else if (scene == 4)
+                {
+                    scene = 26;
+                }
+
+                else if (scene == 6)
+                {
+                    scene = 8;
+                }
+
+                else if (scene == 8)
+                {
+                    scene = 26;
+                }
+                else if (scene == 10)
+                {
+                    scene = 11;
+                }
+
+                else if (scene == 11)
+                {
+                    scene = 26;
+                }
+
+                else if (scene == 12)
+                {
+                    Thread.Sleep(5000);
+                    Refresh();
+
+                    attackValue = randGen.Next(1, 11);
+                    if (attackValue >= 4)
+                    {
+                        scene = 14;
+                    }
+                    else if (attackValue < 4)
+                    {
+                        scene = 13;
+                    }
+                }
+                else if (scene == 13)
+                {
+                    scene = 26;
+                }
+                else if (scene == 15)
+                {
+                    scene = 16;
+                }
+
+                else if (scene == 16)
+                {
+                    scene = 26;
+                }
+
+                else if (scene == 17)
+                {
+                    Thread.Sleep(5000);
+                    Refresh();
+
+                    stealthValue = randGen.Next(1, 11);
+                    if (stealthValue >= 4)
+                    {
+                        scene = 21;
+                    }
+                    else if (stealthValue < 4)
+                    {
+                        scene = 20;
+                    }
+                }
+
+                else if (scene == 20)
+                {
+                    scene = 26;
+                }
+
+                else if (scene == 23)
+                {
+                    scene = 25;
+                }
+
+                else if (scene == 25)
+                {
+                    scene = 26;
+                } 
+
+                else if (scene == 26)
+                {
+                    Application.Exit();        //Closes program when "NO" is selected when asked "PLAY AGAIN?"
+                }
+            }
+
+            else if (e.KeyCode == Keys.Space)  //Green Button press
+            {
+                if (scene == 6)
+                {
+                    scene = 9;
+                }
+
+                else if (scene == 17)
+                {
+                    Thread.Sleep(5000);
+                    Refresh();
+
+                    attackValue = randGen.Next(1, 11);
+                    if (attackValue >= 4)
+                    {
+                        scene = 19;
+                    }
+                    else if (attackValue < 4)
+                    {
+                        scene = 22;
+                    }
+                }
+
+                else if (scene == 22)
+                {
+                    scene = 26;
+                }             
+            }
+
+            switch (scene)  //Switch statements to advance scenes
+            {
+                case 0:
+                    outputLabel.Text = "You wake up in a dark room with a small trap door and a single torch. Do you go into the trap door or stay in the room?";
+                    redLabel.Text = "LEAVE";
+                    blueLabel.Text = "STAY";
+                    break;
+                case 1:
+                    outputLabel.Text = "You drop down into a long hallway and seea light in the distance. Do you return to grab your torch or start walking?";
+                    redLabel.Text = "GET TORCH";
+                    blueLabel.Text = "WALK";
+                    break;
+                case 2:
+                    outputLabel.Text = "You crawl into a ball and win! Congrats!";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    break;
+                case 3:
+                    outputLabel.Text = "You grab your torch and slowly walk down the hallway. You jump the pit. A Native jumps out and attacks you. You fight back.";
+                    redLabel.Text = "PLEASE WAIT";
+                    blueLabel.Text = "";
+                    break;
+                case 4:
+                    outputLabel.Text = "You start walking but end up falling into a pit of spikes left by the Natives. You died.";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    break;
+                case 5:
+                    outputLabel.Text = "You die due to your attack being too weak. Should've taken karate when you were little";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    break;
+                case 6:
+                    outputLabel.Text = "You mercilessly beat the Native and continue to the light. You find a room with a stream and more Natives. Do you, Stealth Kill the Natives, Fight them head on, or use the stream to swim past?";
+                    redLabel.Text = "SWIM";
+                    blueLabel.Text = "FIGHT";
+                    greenLabel.Text = "STEALTH KILL";
+                    break;
+                case 7:
+                    outputLabel.Text = "You get injured by the piranhas but you escape un noticed!";
+                    redLabel.Text = "CONINUE";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 8:
+                    outputLabel.Text = "You die due to another Native named 'Mnbungo' that suck up behind you. You're dead";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    greenLabel.Text = "";
+                    break;
+                case 9:
+                    outputLabel.Text = "You killed them and got away. What did you think was gonna happen?";
+                    redLabel.Text = "CONTINUE";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 10:
+                    outputLabel.Text = "You get across the stream and make your way to a room. A pedestal stands in the center of the room, an artifact is on the pedestal. An exit door stands across the room. Do you take the artifact or make a break for the door?";
+                    redLabel.Text = "TAKE ARTIFACT";
+                    blueLabel.Text = "LEAVE";
+                    greenLabel.Text = "";
+                    break;
+                case 11:
+                    outputLabel.Text = "YOU WON!!!!!!!!!! go away now";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    greenLabel.Text = "";
+                    break;
+                case 12:
+                    outputLabel.Text = "The exit door slams shut but you manage to find a grappling hook and attach it to a grate over head. Natives suddenly approach out of nowhere. Do you fight or climb up the grappling hook?";
+                    redLabel.Text = "CLIMB";
+                    blueLabel.Text = "FIGHT";
+                    greenLabel.Text = "";
+                    break;
+                case 13:
+                    outputLabel.Text = "You get beaten senseless. Good Job. You died";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    greenLabel.Text = "";
+                    break;
+                case 14:
+                    outputLabel.Text = " You kill all of the Natives and start climbing the rope.";
+                    redLabel.Text = "CONTINUE";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 15:
+                    outputLabel.Text = "You climb up the rope and approach a grate. Do you squeeze through the grat or break it open?";
+                    redLabel.Text = "BREAK";
+                    blueLabel.Text = "SQUEEZE THROUGH";
+                    greenLabel.Text = "";
+                    break;
+                case 16:
+                    outputLabel.Text = "HEY FATTY! You tried squeezing through the grate and ended up breaking a rib that pnctured your heart. You Died, probably again!";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    greenLabel.Text = "";
+                    break;
+                case 17:
+                    outputLabel.Text = "You successfully break open the grate and make it to the surface where there are more Natives. Do you fight the Natives head on, sneak past, or stealth kill them all?";
+                    redLabel.Text = "SNEAK";
+                    blueLabel.Text = "STEALTH KILL";
+                    greenLabel.Text = "FIGHT";
+                    break;
+                case 18:
+                    outputLabel.Text = "You sneak past and make a mad dash to the edge of the jungle";
+                    redLabel.Text = "CONTINUE";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 19:
+                    outputLabel.Text = "You killed everyone, stop being so barbaric. THEY COULD AHAVE FAMILIES.";
+                    redLabel.Text = "CONTINUE";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 20:
+                    outputLabel.Text = "YOU DIED. BE QUIETER";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    greenLabel.Text = "";
+                    break;
+                case 21:
+                    outputLabel.Text = "You stealthily killed everyone and make a mad dash to the path ahead. Good Job lil' ninja.";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    greenLabel.Text = "";
+                    break;
+                case 22:
+                    outputLabel.Text = "YAAAAAAAAAAY you died, again! This is so much fun!";
+                    redLabel.Text = "";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "CONTINUE";
+                    break;
+                case 23:
+                    outputLabel.Text = "You now come to a split in the road. Do you, go left or right? Don't mess it up now. only one answer is RIGHT";
+                    redLabel.Text = "RIGHT";
+                    blueLabel.Text = "left";
+                    greenLabel.Text = "";
+                    break;
+                case 24:
+                    outputLabel.Text = "YOU WON!";
+                    redLabel.Text = "CONTINUE";
+                    blueLabel.Text = "";
+                    greenLabel.Text = "";
+                    break;
+                case 25:
+                    outputLabel.Text = "Shoulda taken my hint dummy!";
+                    redLabel.Text = "";
+                    blueLabel.Text = "CONTINUE";
+                    greenLabel.Text = "";
+                    break;
+                case 26:
+                    outputLabel.Text = "PLAY AGAIN?";
+                    redLabel.Text = "YES!";
+                    blueLabel.Text = "NO.";
+                    greenLabel.Text = "";
+                    break;
+                default:
+                    outputLabel.Text = "You wake up in a dark room with a small trap door and a single torch. Do you go into the trap door or stay in the room?";
+                    redLabel.Text = "LEAVE";
+                    blueLabel.Text = "STAY";
+                    break;
+            }
+        }
+    }
+}
